ad1d3b8f6009f3073801773d0a4053fdd5dec153
mohsin e nur <Nur Mohsin>
2018-12-02 12:50:35 +0600

    api GET request working

--- restful.py
+++ restful.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+languages = [{'name': 'Python'}, {'name': 'Java'}, {'name': 'C++'}]
 
-@app.route('/', methods=['GET', ])
+
+@app.route('/', methods=['GET'])
 def test():
-	return jsonify({'message' : 'It works'})
+	return jsonify({'message': 'It works'})
+
+
+@app.route('/lang/', methods=['GET'])
+def return_all():
+	return jsonify({'language': languages})
+
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def return_one(name):
+	lang = [language for language in languages if language['name'] == name]
+	return jsonify({'language': lang[0]})
 
 
 if __name__ == '__main__':
